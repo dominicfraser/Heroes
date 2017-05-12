8325815434cf342beed7e21b8c761167544ce938
Dominic <mrdjfraser@gmail.com>
2017-05-12 14:34:12 +0100

    basic food tests passing

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -1,31 +1,18 @@
 var assert = require('assert')
-var Task = require('../task.js')
+var Food = require('../food.js')
 
-describe('Task', function () {
+describe('Food', function () {
 
   beforeEach(function(){
-    this.task1 = new Task("Hard","High",100);
+    this.food1 = new Food("Apple", 10);
   });
 
-  it("should have a difficulty", function(){
-    assert.equal("Hard", this.task1.difficulty);
+  it("should have a name", function(){
+    assert.equal("Apple", this.food1.name);
   });
 
-  it("should have a urgency", function(){
-    assert.equal("High", this.task1.urgency);
-  });
-
-  it("should have a reward", function(){
-    assert.equal(100, this.task1.reward);
-  });
-
-  it("should have a default completed of false", function(){
-    assert.equal(false, this.task1.completed);
-  });
-
-  it("should be able to change completed", function(){
-    this.task1.markCompleted();
-    assert.equal(true, this.task1.completed);
+  it("should have a replenishment value", function(){
+    assert.equal(10, this.food1.replenishment);
   });
 
 });
